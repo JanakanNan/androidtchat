c25ac356989d3610fbb0bc176e5adf11ee527ec0
JanakanNan <j.nandakumaran@insta.fr>
2018-04-23 01:41:36 +0200

    Mise en place de tous les layout

---
+++ app/src/main/java/com/example/janakannandakumaran/tchat/Constants.java
@@ -0,0 +1,11 @@
+package com.example.janakannandakumaran.tchat;
+
+public class Constants {
+
+    public static final String USERS_DB = "users";
+    public static final String USERNAMES_DB = "usernames";
+
+    private Constants(){
+
+    }
+}

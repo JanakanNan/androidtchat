ece905b36d1711d9206e9e36599436f82aedf9c9
JanakanNan <j.nandakumaran@insta.fr>
2018-04-23 00:53:19 +0200

    Login page

--- app/src/main/java/com/example/janakannandakumaran/tchat/LoginActivity.java
+++ app/src/main/java/com/example/janakannandakumaran/tchat/LoginActivity.java
@@ -1,13 +1,155 @@
 package com.example.janakannandakumaran.tchat;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.nfc.Tag;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.example.janakannandakumaran.tchat.Entities.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText etPseudo;
+    private ProgressBar loader;
+    private Button btnLogin;
+
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mRef;
+    private SharedPreferences prefs;
+
+    private static final String TAG = "Tchat";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Initialisation des vues
+        etPseudo = (EditText) findViewById(R.id.etPseudo);
+        loader = (ProgressBar) findViewById(R.id.loader);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(this);
+
+        //Init de firebase
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance();
+        mRef = mDatabase.getReference();
+
+        prefs = getSharedPreferences("tchat", MODE_PRIVATE);
+
+        if(mAuth.getCurrentUser() != null && prefs.getString("PSEUDO", null) != null)
+        {
+            startActivity(new Intent(getApplicationContext(), TchatActivity.class));
+            finish();
+        }
+    }
+
+        private void checkUsername (final String username,final CheckUsernameCallback callback){
+            mRef.child("usernames").child(username).addListenerForSingleValueEvent(new ValueEventListener() {
+
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    if(dataSnapshot.getValue() != null)
+                    {
+                        callback.isTaken();
+                    }else {
+                        callback.isValid(username);
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    Log.w(TAG, "onCancelled: "+databaseError.getMessage());
+                    loader.setVisibility(View.INVISIBLE);
+
+                }
+            });
+        }
+
+
+    private void registerUser (final String username) {
+
+        mAuth.signInAnonymously().addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (!task.isSuccessful()) {
+                    Toast.makeText(LoginActivity.this, "Connexion impossible, Veuillez réessayer", Toast.LENGTH_SHORT).show();
+
+                }
+                loader.setVisibility(View.INVISIBLE);
+
+
+                final String userId = task.getResult().getUser().getUid();
+
+                checkUsername(username, new CheckUsernameCallback(){
+                    @Override
+                    public void isValid(final String username)
+                    {
+                        User newUser = new User(username, userId);
+                        mRef.child("users").child(userId).setValue(newUser).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if ((task.isSuccessful()))
+                                {
+                                    mRef.child("usernames").child(username).setValue(userId);
+                                    prefs.edit().putString("PSEUDO",username).apply();
+                                    startActivity(new Intent(getApplicationContext(), TchatActivity.class));
+                                    finish();
+                                }
+                            }
+                        });
+
+                    }
+
+                    @Override
+                    public void isTaken() {
+                        Toast.makeText(LoginActivity.this, "Veuillez choisir un autre pseudo", Toast.LENGTH_SHORT).show();
+                        loader.setVisibility(View.INVISIBLE);
+                    }
+                });
+            }
+        });
+    }
+
+
+
+    @Override
+    public void onClick(View view) {
+        loader.setVisibility(view.VISIBLE);
+
+        String username = etPseudo.getText().toString();
+
+        if (!TextUtils.isEmpty(username))
+        {
+            registerUser(username);
+        }
+
+    }
+
+    interface CheckUsernameCallback{
+        void isValid(String username);
+        void isTaken();
+
     }
 }
